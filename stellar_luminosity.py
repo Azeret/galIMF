5f2b67c8743e0746e228ea427f3eff7c4b71a139
zhiqiangyan <beijingjuzikong@gmail.com>
2019-05-30 16:22:01 +0200

    change text

--- stellar_luminosity.py
+++ stellar_luminosity.py
@@ -1,6 +1,6 @@
-# Here we assume a simplified form using only the main-sequence luminosity as a function of mass
-# See Yan et al. 2019 for details
-# The stellar luminosity should also be a function of Y_for_helium, Z_for_metal, age which shall be added later
+# Here we assume a simplified form using only the main-sequence luminosity as a function of mass.
+# See Yan et al. 2019 for details.
+# The stellar luminosity should also be a function of Y_for_helium and Z_for_metal, which shall be added later.
 
 
 def stellar_luminosity_function(mass):
