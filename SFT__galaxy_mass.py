9b45b5ba9f35af15b26e0cfc92da4e0d6a91a14a
zhiqiangyan <beijingjuzikong@gmail.com>
2019-06-07 13:27:57 +0200

    add comment

--- SFT__galaxy_mass.py
+++ SFT__galaxy_mass.py
@@ -29,7 +29,7 @@ def simulate(imf, Log_SFR, SFEN, STF):
         mass_boundary_observe_low=1.5,
         mass_boundary_observe_up=8,
         SFH_model='provided',
-        SFE=0.013,
+        SFE=0.013,  # This parameter is not applied when SFH_model='provided'.
         SNIa_ON=True,
         high_time_resolution=None,
         plot_show=None,
@@ -81,6 +81,7 @@ def simulate(imf, Log_SFR, SFEN, STF):
 if __name__ == '__main__':
 
     for SFEN in [25, 50, 100, 200, 400, 800]:
+        print('Start simulations for SFT={}!'.format(SFEN))
         for STF in [0.4, 0.3, 0.2]:
             for Log_SFR in [-1.0, 0.0, 1.0, 2.0, 3.0]:
                 for imf in ['igimf', 'Kroupa']:
