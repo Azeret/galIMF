ff30dec7360315f44dac1ca45d9095ad36b937d7
juzikong <beijingjuzikong@gmail.com>
2020-07-24 16:07:22 +0200

    Update Kroupa_IMF.py
    normalized to a population with mass = 1 Msun

--- IMFs/Kroupa_IMF.py
+++ IMFs/Kroupa_IMF.py
@@ -1,9 +1,35 @@
-def custom_imf(mass, time=0):  # there is no time dependence for Kroupa IMF
+from scipy.integrate import quad
+
+alpha3 = 2.3
+
+def custom_imf_unnormalized(mass):  # there is no time dependence for Kroupa IMF
     if mass < 0.08:
         return 0
     elif mass < 0.5:
         return 2*mass**(-1.3)
-    elif mass < 150:
+    elif mass < 1:
         return mass**(-2.3)
+    elif mass < 150:
+        return mass**(-alpha3)
+    else:
+        return 0
+
+
+def mass_function(mass):
+    return custom_imf_unnormalized(mass) * mass
+
+
+integrated_mass = quad(mass_function, 0.08, 150, limit=50)[0]
+
+
+def custom_imf(mass, time=0):  # normalized to a population with mass = 1 Msun
+    if mass < 0.08:
+        return 0
+    elif mass < 0.5:
+        return 2*mass**(-1.3)/integrated_mass
+    elif mass < 1:
+        return mass**(-2.3)/integrated_mass
+    elif mass < 150:
+        return mass**(-alpha3)/integrated_mass
     else:
         return 0
