825c1b9c3de2e8d5a1ed3cface2cb84dc4c727ef
juzikong <beijingjuzikong@gmail.com>
2020-07-24 16:12:11 +0200

    Update Salpeter_IMF.py
    Normalized to a population with mass = 1 Msun

--- IMFs/Salpeter_IMF.py
+++ IMFs/Salpeter_IMF.py
@@ -1,7 +1,26 @@
-def custom_imf(mass, time=0):  # there is no time dependence for Salpeter IMF
+from scipy.integrate import quad
+
+
+def custom_imf_unnormalized(mass):  # there is no time dependence for Salpeter IMF
     if mass < 0.1:
         return 0
     elif mass < 100:
         return mass ** (-2.35)
+    else:
+        return 0
+
+
+def mass_function(mass):
+    return custom_imf_unnormalized(mass) * mass
+
+
+integrated_mass = quad(mass_function, 0.08, 150, limit=50)[0]
+
+
+def custom_imf(mass, time=0):  # normalized to a population with mass = 1 Msun
+    if mass < 0.1:
+        return 0
+    elif mass < 100:
+        return mass ** (-2.35)/integrated_mass
     else:
         return 0
