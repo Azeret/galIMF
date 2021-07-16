7e061262f24ceeb3c952460d4b813629c7a0a3de
juzikong <beijingjuzikong@gmail.com>
2021-07-16 16:24:43 +0800

    Update galimf.py
    add comment
    "Warning: Abnormally high gas metallicity leading to an unrealistic IMF shape according to the assumed variation law: alpha2_model == 'Z'. Please check your galaxy evolution settings or change to a different IMF variation assumption."

--- galimf.py
+++ galimf.py
@@ -983,6 +983,8 @@ def function_alpha_2_change(alpha_2, alpha2_model, M_over_H):
         return alpha_2_change
     elif (alpha2_model == 'Z'):
         alpha_2_change = alpha_2 + 63 * (10**M_over_H - 1) * 0.0142
+        if M_over_H>1:
+            print("Warning: Abnormally high gas metallicity leading to an unrealistic IMF shape according to the assumed variation law: alpha2_model == 'Z'. Please check your galaxy evolution settings or change to a different IMF variation assumption.")
         return alpha_2_change
     elif (alpha2_model == 'IGIMF2.5'):
         alpha_2_change = alpha_2 + 0.12 * M_over_H
